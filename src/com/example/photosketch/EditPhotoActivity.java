03d34babbe50f9208dc99fd2c4af33e6ca7ac7a7
Damian Cudek <damian.cudek@gmail.com>
2015-01-20 09:28:47 +0100

    Dodawanie robienia zdjęcia.

---
+++ src/com/example/photosketch/EditPhotoActivity.java
@@ -0,0 +1,16 @@
+package com.example.photosketch;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class EditPhotoActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_edit_photo);
+		
+	}
+}
--- src/com/example/photosketch/MainActivity.java
+++ src/com/example/photosketch/MainActivity.java
@@ -1,15 +1,62 @@
 package com.example.photosketch;
 
+
+
 import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 public class MainActivity extends Activity {
+
+	static final int REQUEST_IMAGE_CAPTURE = 1;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		Button makePhoto = (Button) findViewById(R.id.makePhotoButton);
+		makePhoto.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+				if (intent.resolveActivity(getPackageManager()) != null) {
+					startActivityForResult(intent, REQUEST_IMAGE_CAPTURE);
+				}
+			}
+		});
+		
+		Button editPhoto = (Button)findViewById(R.id.editPhotoButton);
+		editPhoto.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent(getApplicationContext(),EditPhotoActivity.class);
+				startActivity(intent);
+				
+				
+			}
+		});
+
+	}
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+			Bundle extras = data.getExtras();
+			Bitmap imageBitmap = (Bitmap) extras.get("data");
+			ImageView imageView = (ImageView) findViewById(R.id.imageView1);
+			imageView.setImageBitmap(imageBitmap);
+		}
 	}
 }
